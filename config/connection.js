72a5747507ca5827a405750659afb2e74bb24d21
Your Name <you@example.com>
2019-05-14 22:14:32 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Root",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -17,6 +21,8 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
